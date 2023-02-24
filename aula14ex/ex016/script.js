934044651fd89e1d9341b3411c17c43244438c50
deivisson lopes <dslvr87@gmail.com>
2023-02-24 20:55:17 -0300

    Exercício 26 de media query

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,39 @@
-alert('Olá!')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO!] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <=0 ) {
+            window.alert('Passo inválido! Considerando passso 1')
+            p = 1
+        }
+
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} 😂`
+    
+            }
+            
+        } else {
+            for (let c = i; c >= f; c -= p ) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `🎌`
+        
+    }
+}
+
+
+
+
+
